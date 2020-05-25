de5a51dfdba9dca1c3be1415ec6677431b8c4ced
Manav Mehta <tmanavmehta@gmail.com>
2020-05-25 23:50:22 +0530

    context-menu: create custom context-menu for zulip-desktop
    The context-menu will serve as the universal context-menu for zulip-dekstop app
    * It performs the appropriate operations
    * Provides suggestions for misspellings caught by electron's built-in spellchecker
    * Will be maintained by the organisation

---
+++ app/renderer/js/components/context-menu.ts
@@ -0,0 +1,102 @@
+import {remote} from 'electron';
+import WebView from './webview';
+import handleExternalLink from './handle-external-link';
+import * as t from '../utils/translation-util';
+const {clipboard, Menu, MenuItem} = remote;
+
+export const contextMenu = (webview: WebView, event: any) => {
+	const webContents = webview.$el.getWebContents();
+	const props = event.params;
+	const isText = Boolean(props.selectionText.length);
+	const isLink = Boolean(props.linkURL);
+
+	const getContextMenu = (): Electron.MenuItemConstructorOptions[] => {
+		return [{
+			label: t.__('Add to Dictionary'),
+			visible: props.isEditable && isText && props.misspelledWord,
+			click(_item) {
+				webContents.session.addWordToSpellCheckerDictionary(props.misspelledWord);
+			}
+		}, {
+			type: 'separator'
+		}, {
+			label: t.__(`Look Up "${(props.selectionText as string)}"`),
+			visible: process.platform === 'darwin' && isText,
+			click(_item) {
+				webContents.showDefinitionForSelection();
+			}
+		}, {
+			type: 'separator'
+		}, {
+			label: t.__('Cut'),
+			visible: isText,	//	Bug: visible and enabled are not working as expected
+			enabled: props.isEditable,
+			role: 'cut'
+		}, {
+			label: t.__('Copy'),
+			role: 'copy'
+		}, {
+			label: t.__('Paste'),	// Bug : Pastes upon the selected _item
+			role: 'paste'
+		}, {
+			type: 'separator'
+		}, {
+			label: t.__('Copy Link'),
+			visible: isText && isLink,
+			click(_item) {
+				clipboard.write({
+					bookmark: props.linkText,
+					text: props.linkURL
+				});
+			}
+		}, {
+			label: t.__('Open Link'), // Not working as of now
+			visible: isText && isLink,
+			click(_item) {
+				handleExternalLink.call(webview, event);
+			}
+		}, {
+			label: t.__('Copy Image'),
+			visible: props.mediaType === 'image',
+			click(_item) {
+				webContents.copyImageAt(props.x, props.y);
+			}
+		}, {
+			label: t.__('Copy Image URL'),
+			visible: props.mediaType === 'image',
+			click(_item) {
+				clipboard.write({
+					bookmark: props.srcURL,
+					text: props.srcURL
+				});
+			}
+		}];
+	};
+
+	const makeSuggestion = (suggestion: string) => {
+		return new MenuItem({
+			label: suggestion,
+			async click() {
+				await webContents.insertText(suggestion);
+			}
+		});
+	};
+
+	const ctx = getContextMenu();
+	const menu = Menu.buildFromTemplate(ctx);
+
+	if (props.misspelledWord) {
+		if (props.dictionarySuggestions.length > 0) {
+			Object.values(props.dictionarySuggestions).forEach(value => {
+				menu.insert(0, makeSuggestion(value as string));
+			});
+		} else {
+			menu.insert(0, new MenuItem({
+				label: 'No Suggestion Found',
+				enabled: false
+			}));
+		}
+	}
+
+	menu.popup();
+};
