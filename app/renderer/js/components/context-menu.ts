0e15c910abdfe3270e6b166fa3b06393df1488d3
Manav Mehta <tmanavmehta@gmail.com>
2020-06-02 12:56:42 +0530

    Refactors for the code:
    * Correct the appearance order of suggestions
    * Add `selectionText` to Add to Dictionary option
    * Remove redundant OpenLink option

--- app/renderer/js/components/context-menu.ts
+++ app/renderer/js/components/context-menu.ts
@@ -1,8 +1,7 @@
 import {remote} from 'electron';
 import WebView from './webview';
-import handleExternalLink from './handle-external-link';
 import * as t from '../utils/translation-util';
-const {clipboard, Menu, MenuItem} = remote;
+const {clipboard, Menu} = remote;
 
 export const contextMenu = (webview: WebView, event: any) => {
 	const webContents = webview.$el.getWebContents();
@@ -10,103 +9,93 @@ export const contextMenu = (webview: WebView, event: any) => {
 	const isText = Boolean(props.selectionText.length);
 	const isLink = Boolean(props.linkURL);
 
-	const getContextMenu = (): Electron.MenuItemConstructorOptions[] => {
-		return [{
-			label: t.__('Add to Dictionary'),
-			visible: props.isEditable && isText && props.misspelledWord,
-			click(_item) {
-				webContents.session.addWordToSpellCheckerDictionary(props.misspelledWord);
-			}
-		}, {
-			type: 'separator'
-		}, {
-			label: t.__(`Look Up "${(props.selectionText as string)}"`),
-			visible: process.platform === 'darwin' && isText,
-			click(_item) {
-				webContents.showDefinitionForSelection();
-			}
-		}, {
-			type: 'separator'
-		}, {
-			label: t.__('Cut'),
-			visible: isText,
-			enabled: props.isEditable,
-			accelerator: 'CommandOrControl+X',
-			click(_item){
-				webContents.cut();
-			}
-		}, {
-			label: t.__('Copy'),
-			accelerator: 'CommandOrControl+C',
-			click(_item){
-				webContents.copy();
-			}
-		}, {
-			label: t.__('Paste'), // Bug: Paste replaces text
-			accelerator: 'CommandOrControl+V',
-			enabled: props.isEditable,
-			click(){
-				webContents.paste();
-			}
-		}, {
-			type: 'separator'
-		}, {
-			label: t.__('Copy Link'),
-			visible: isText && isLink,
-			click(_item) {
-				clipboard.write({
-					bookmark: props.linkText,
-					text: props.linkURL
-				});
-			}
-		}, {
-			label: t.__('Open Link'), // Not working as of now
-			visible: isText && isLink,
-			click(_item) {
-				handleExternalLink.call(webview, event);
-			}
-		}, {
-			label: t.__('Copy Image'),
-			visible: props.mediaType === 'image',
-			click(_item) {
-				webContents.copyImageAt(props.x, props.y);
-			}
-		}, {
-			label: t.__('Copy Image URL'),
-			visible: props.mediaType === 'image',
-			click(_item) {
-				clipboard.write({
-					bookmark: props.srcURL,
-					text: props.srcURL
-				});
-			}
-		}];
-	};
-
 	const makeSuggestion = (suggestion: string) => {
-		return new MenuItem({
+		return {
 			label: suggestion,
+			visible: true,
 			async click() {
 				await webContents.insertText(suggestion);
 			}
-		});
+		};
 	};
 
-	const ctx = getContextMenu();
-	const menu = Menu.buildFromTemplate(ctx);
+	let menuTemplate: Electron.MenuItemConstructorOptions[] = [{
+		label: t.__(`Add "${(props.selectionText as string)}" to Dictionary`),
+		visible: props.isEditable && isText && props.misspelledWord,
+		click(_item) {
+			webContents.session.addWordToSpellCheckerDictionary(props.misspelledWord);
+		}
+	}, {
+		type: 'separator'
+	}, {
+		label: t.__(`Look Up "${(props.selectionText as string)}"`),
+		visible: process.platform === 'darwin' && isText,
+		click(_item) {
+			webContents.showDefinitionForSelection();
+		}
+	}, {
+		type: 'separator'
+	}, {
+		label: t.__('Cut'),
+		visible: isText,
+		enabled: props.isEditable,
+		accelerator: 'CommandOrControl+X',
+		click(_item) {
+			webContents.cut();
+		}
+	}, {
+		label: t.__('Copy'),
+		accelerator: 'CommandOrControl+C',
+		click(_item) {
+			webContents.copy();
+		}
+	}, {
+		label: t.__('Paste'), // Bug: Paste replaces text
+		accelerator: 'CommandOrControl+V',
+		enabled: props.isEditable,
+		click() {
+			webContents.paste();
+		}
+	}, {
+		type: 'separator'
+	}, {
+		label: t.__('Copy Link'),
+		visible: isText && isLink,
+		click(_item) {
+			clipboard.write({
+				bookmark: props.linkText,
+				text: props.linkURL
+			});
+		}
+	}, {
+		label: t.__('Copy Image'),
+		visible: props.mediaType === 'image',
+		click(_item) {
+			webContents.copyImageAt(props.x, props.y);
+		}
+	}, {
+		label: t.__('Copy Image URL'),
+		visible: props.mediaType === 'image',
+		click(_item) {
+			clipboard.write({
+				bookmark: props.srcURL,
+				text: props.srcURL
+			});
+		}
+	}];
 
 	if (props.misspelledWord) {
 		if (props.dictionarySuggestions.length > 0) {
-			Object.values(props.dictionarySuggestions).forEach(value => {
-				menu.insert(0, makeSuggestion(value as string));
-			});
+			const suggestions: Electron.MenuItemConstructorOptions[] = props.dictionarySuggestions.map((suggestion: string) => makeSuggestion(suggestion));
+			menuTemplate = suggestions.concat(menuTemplate);
 		} else {
-			menu.insert(0, new MenuItem({
+			menuTemplate.unshift({
 				label: 'No Suggestion Found',
 				enabled: false
-			}));
+			});
 		}
 	}
-	console.log(props.dictionarySuggestions);
+
+	const menu = Menu.buildFromTemplate(menuTemplate);
 	menu.popup();
 };
